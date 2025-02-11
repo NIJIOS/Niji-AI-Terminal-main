8639223f9a3cbbe93ff6e838db644d1984466ace
NIJIOS <nijiosfun@gmail.com>
2025-02-11 21:27:17 +0800

    Add files via upload

---
+++ src/components/NijiLogo.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export function NijiLogo({ className = '' }: { className?: string }) {
+  return (
+    <img 
+      src=""
+      alt="nijiAI Logo"
+      className={className}
+    />
+  );
+}
